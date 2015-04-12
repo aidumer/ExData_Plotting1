60feee7c8b9a99a382028445133b1b09b15e1dfe
aidumer <alex.dumer@gmail.com>
2015-04-12 18:20:57 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,13 @@
+setwd("C:/Alex/Coursera")
+library(data.table)
+
+power <- fread("C:/Alex/Coursera/household_power_consumption.csv", sep = ";", header = TRUE, nrow = 2075259)
+power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
+mydata <- subset(power,Date >= "2007-02-01" & Date <= "2007-02-02")
+mydata$Date <- as.Date(mydata$Date)
+mydata$datetime <- paste(mydata$Date, mydata$Time)
+mydata$DateTime <- as.POSIXct(mydata$datetime)
+head(mydata)
+plot(mydata$DateTime, mydata$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
